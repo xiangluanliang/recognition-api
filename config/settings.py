7ac13edba4598c8f13e49bf5086c207eaa5cfae9
Yhr <23301142@bjtu.edu.cn>
2025-07-07 14:46:08 +0800

    用户登录

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_yasg',
     'corsheaders',
     'api'
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from rest_framework.authtoken.views import obtain_auth_token
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -24,4 +25,5 @@ urlpatterns = [
     # --- Swagger UI 路由 ---
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
