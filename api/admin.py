460ec9ac771ea2e7652f21adec2f7737a8a36037
n-Frederic <xuechaozhou001@gmail.com>
2025-07-08 11:10:58 +0800

    update admin.py
    增加数据库操纵模块

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,26 @@
 # api/admin.py
 from django.contrib import admin
-from .models import User
+from .models import (
+    User,
+    OperationLog,
+    Subject,
+    RecognitionLog,
+    DetectionLog,
+    WarningZone,
+    IncidentType,
+    IncidentDetectionLog,
+    Camera,
+    AlarmLog,
+)
 
 admin.site.register(User)
+admin.site.register(OperationLog)
+admin.site.register(Subject)
+admin.site.register(RecognitionLog)
+admin.site.register(DetectionLog)
+admin.site.register(WarningZone)
+admin.site.register(IncidentType)
+admin.site.register(IncidentDetectionLog)
+admin.site.register(Camera)
+admin.site.register(AlarmLog)
+
