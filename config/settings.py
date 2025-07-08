c329bd79ea549ec9054540ba2cecbfca4154554d
Yhr <23301142@bjtu.edu.cn>
2025-07-08 14:36:50 +0800

    更换数据库

--- config/settings.py
+++ config/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rg_db',
+        'USER': 'rg_user',
+        'PASSWORD': 'Zyjjh0707',
+        'HOST': '8.152.101.217',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
