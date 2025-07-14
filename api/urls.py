517942ff05b61c003ae68012cf9e7d49c624580f
Yhr <23301142@bjtu.edu.cn>
2025-07-14 10:43:06 +0800

    daily_report

--- api/urls.py
+++ api/urls.py
@@ -32,7 +32,7 @@ router.register(r'warning_zones', WarningZoneViewSet)
 router.register(r'cameras', CameraViewSet)
 # router.register(r'alarm_logs', AlarmLogViewSet)
 router.register(r'event_logs', EventLogViewSet)
-# router.register('daily_report', DailyReportDataAPI, basename='daily_report')
+router.register(r'daily_report', DailyReportDataAPI, basename='daily_report')
 # router.register(r'feedback', FeedbackView)
 
 urlpatterns = [
