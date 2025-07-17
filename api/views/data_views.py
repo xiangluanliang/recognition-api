cd9c5e8f781e9da8c365aaf74c93431af764b3dd
n-Frederic <xuechaozhou001@gmail.com>
2025-07-18 03:01:22 +0800

    update data_views.py

--- api/views/data_views.py
+++ api/views/data_views.py
@@ -311,7 +311,7 @@ class DailyReportDataAPI(APIView):
             processing_counts = alarms_today.filter(status=1).values('event__event_type').annotate(count=Count('id'))
             processed_counts = alarms_today.filter(status=2).values('event__event_type').annotate(count=Count('id'))
             # 摄像头覆盖数量
-            camera_counts = alarms_today.values('event__event_type', 'camera__id').distinct().values('event__event_type').annotate(cameras=Count('camera__id'))
+            camera_counts = alarms_today.values('event__event_type', 'event__camera').distinct().values('event__event_type').annotate(cameras=Count('event__camera'))
 
             # 整合统计数据
             event_stats = {}
